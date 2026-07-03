2a297e8acb3b53cd1275ba87ad9037bff9c16ea6
Teneti Srujana <161198620+TenetiSrujana@users.noreply.github.com>
2026-07-03 20:58:08 +0530

    Time: 845 ms (18.51%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,13 @@
+SELECT Sales.product_id,
+       Sales.year AS first_year,
+       Sales.quantity,
+       Sales.price
+FROM Sales
+JOIN (
+    SELECT product_id,
+           MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+) FirstYear
+ON Sales.product_id = FirstYear.product_id
+AND Sales.year = FirstYear.first_year;
